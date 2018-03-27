94e0f628689f6fa85650c91738e39a230be8b85e
yungwifi <smerryman@me.com>
2018-03-27 11:26:34 -0400

    got the game memory logic working as well as arandom square selector function when game starts

--- main.js
+++ main.js
@@ -2,59 +2,59 @@
 
 const squareOptions = ['red', 'blue', 'yellow', 'green']
 const gameMemory = []
+let randomSquare;
 //random square generator 
-const randomSquare = function () {
-    return squareOptions[Math.floor(Math.random() * squareOptions.length)]
-}
-
-const colorReset = function () {
-    setTimeout(function () {
-        document.getElementsByClassName('piece')
-    }, 3000);
+const squareSelector = function () {
+    randomSquare = squareOptions[Math.floor(Math.random() * squareOptions.length)]
 
 }
+
 
 //user clicks square 
-const userClick = $('.piece').click(() => {
+userClick = $('.piece').click(() => {
     console.log('user clicked')
 })
-const userClicksRed = $('#red').click(() => {
+userClicksRed = $('#red').click(() => {
     $('#red').css("background-color", "black")
+    setTimeout($.proxy(userClicksRed, 'jQuery'), 1000)
 })
-const userClicksYellow = $('#yellow').click(() => {
+userClicksYellow = $('#yellow').click(() => {
     $('#yellow').css("background-color", "black")
 })
-const userClicksGreen = $('#green').click(() => {
+userClicksGreen = $('#green').click(() => {
     $('#green').css("background-color", "black")
 })
-const userClicksBlue = $('#blue').click(() => {
+userClicksBlue = $('#blue').click(() => {
     $('#blue').css("background-color", "black")
 })
 
 
 //AI displays randomly selected sqaure 
-const displaySquare = function () {
-    var r = $('#red')
-    if (r === 'red') {
-        $('#red').css("background-color", "black")
-    } else if (randomSquare === 'yellow') {
+displaySquare = function () {
+    if (randomSquare == '') {
+        userClicksRed()
+    } else if (randomSquare == 'yellow') {
         return userClicksYellow
-    } else if (randomSquare === 'blue') {
+    } else if (randomSquare == 'blue') {
         return userClicksBlue
-    } else if (randomSquare === 'green') {
+    } else if (randomSquare == 'green') {
         return userClicksGreen
     }
 }
 
 //user clicks start and game begins 
 $('#start').click(() => {
-    console.log(randomSquare())
-    gameMemory.push(randomSquare())
-    colorReset(userClicksRed)
+    squareSelector()
+    gameMemory.push(randomSquare)
+    console.log(randomSquare)
 })
+
+displaySquare();
+
+
 //how do I limit these clicks to only one event per session? 
+//why is my gameMemory push different from my randomsquare generator in console
 
 $(document).ready()
-console.log("Start Game")
-
+console.log('Start Game')
 
